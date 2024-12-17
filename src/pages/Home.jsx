7832b4fc121289b1fa78e2864dcc572f591745d3
aamirk0008 <113338901+aamirk0008@users.noreply.github.com>
2024-12-17 19:50:47 +0530

    Homepage 2.0.8

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -33,7 +33,7 @@ const Home = () => {
       <CategoryFeat/>
       <MemoryCard/>
       <Trending/>
-      <InviteSection/>
+      {/* <InviteSection/> */}
 
       <VideoCard/>
       <SurpriseCard/>
