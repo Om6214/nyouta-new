eedb603929932fe2ae18203821f1b6970ef6fc82
Anurag Patel <118664591+anurag-2024@users.noreply.github.com>
2024-12-24 01:46:57 +0530

    addtocart

--- backend-architecture/models/Cart.js
+++ backend-architecture/models/Cart.js
@@ -2,7 +2,14 @@ import mongoose from 'mongoose';
 
 const CartSchema = new mongoose.Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  products: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true }],
+  products: [{
+    productId:{
+     type: mongoose.Schema.Types.ObjectId, ref: 'Product'
+    },
+    quantity:{
+      type:Number
+    }
+   }],
   totalPrice: { type: Number, required: true },
 });
 
