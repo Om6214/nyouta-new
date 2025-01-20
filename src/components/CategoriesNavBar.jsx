377393fe31eecc05d788c61151c793d7830be88c
RoshanTambe44 <roshantambe675@gmail.com>
2025-01-20 21:22:17 +0530

    add chenges

--- src/components/CategoriesNavBar.jsx
+++ src/components/CategoriesNavBar.jsx
@@ -464,7 +464,11 @@ const CategoriesNavBar = () => {
         }, {
           subTitle: "Wedding Notepad",
           imgUrl: "https://res.cloudinary.com/dpesh4axn/image/upload/v1736846734/kvsupuz7sbivojh4aind.jpg", // Example image
-          url: "e/nav/Planner Books/Planner Books/Wedding Notepad"
+          url: "e/nav/Planner Books/Planner Books/Wedding Notepad(liner)"
+        }, {
+          subTitle: "Wedding Notepad",
+          imgUrl: "https://res.cloudinary.com/dpesh4axn/image/upload/v1736846734/kvsupuz7sbivojh4aind.jpg", // Example image
+          url: "e/nav/Planner Books/Planner Books/Wedding Notepad(photo)"
         }, {
           subTitle: "Guest List Booklet - Best Seller",
           imgUrl:
