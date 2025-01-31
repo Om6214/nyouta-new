2ec7f4b691434a09c1f3911c447922c0488d16d9
Om6214 <omnathganapure9981@gmail.com>
2025-01-31 23:37:22 +0530

    minor

--- src/components/FreeGreetingFilter.jsx
+++ src/components/FreeGreetingFilter.jsx
@@ -418,7 +418,7 @@ export default function CategoryFilterLabel() {
 
               {/* Main Content */}
               <div className="w-full lg:w-4/5">
-                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+                <div className="grid px-8 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
                   {currentItems.length > 0 ? (
                     currentItems.map((item, index) => (
                       <Link
